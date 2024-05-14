5fe3a3eadb03188fd2184ee58e915975e925b526
fahlanlogic <109933376+fahlanlogic@users.noreply.github.com>
2024-05-14 22:41:49 +0700

    Fahdi/card product (#6)
    * create carousel component ui
    
    * add effect and adjust height
    
    * bug fixing
    
    * create card product and example in home (UI)

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,3 +1,4 @@
+import CardProduct from "@/components/CardProduct";
 import Carousel from "@/components/Carousel";
 import Image from "next/image";
 
@@ -5,6 +6,11 @@ export default function Home() {
   return (
     <main className="h-[9999px] xl:max-w-6xl mx-auto px-4 xl:px-0">
       <Carousel />
+    
+      {/* contoh penggunaan card menggunakan grid */}
+      <div className="grid xl:grid-flow-col grid-cols-2 auto-cols-max gap-3 sm:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5">
+        <CardProduct />
+      </div>
       <h1>Home</h1>
     </main>
   );
