4be26da8c31225213d75cadd77aeb08afab0d86d
fahlanlogic <109933376+fahlanlogic@users.noreply.github.com>
2024-05-20 19:01:33 +0700

    Fahdi/cart page (#15)
    * create UI cart page
    
    * create UI checkout page
    
    * add functionality cart page for mobile only
    
    * Update README.md
    
    * add warehouse
    
    * revisi note in readme.md
    
    * fixing dashboard layout and navbar
    
    * fetch api and add functionality
    
    * fixing price
    
    * moving file fetching
    
    * delete checkout route crash

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,6 +11,12 @@ module.exports = {
       fontFamily: {
         sans: ["Cairo", "sans-serif"],
       },
+      boxShadow: {
+        t: "0px -2px 3px 1px rgba(0, 0, 0, 0.1)",
+      },
+      animation: {
+        "spin-slow": "spin 3s linear infinite",
+      },
     },
     daisyui: {
       themes: ["light", "dark", "cupcake"],
