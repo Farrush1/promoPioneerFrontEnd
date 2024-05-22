9a39bb5077cc7392f3cb56b1c76c58c24ce33582
fahlan logic <alanfahdi@gmail.com>
2024-05-22 17:00:51 +0700

    func : add functionality edit address

--- src/app/checkout/[id]/page.jsx
+++ src/app/checkout/[id]/page.jsx
@@ -7,8 +7,12 @@ import { BiLoaderCircle } from "react-icons/bi";
 
 export default function Checkout({ params: { id } }) {
   const [checkoutList, setCheckoutList] = useState([]);
+  const [citiesList, setCitiesList] = useState([]);
   const [loading, setLoading] = useState(true);
-  console.log(checkoutList);
+  const [selectedCity, setSelectedCity] = useState("");
+  const [fullAddress, setFullAddress] = useState("");
+  const [bioList, setBioList] = useState({});
+  console.log(selectedCity);
 
   useEffect(() => {
     setLoading(true);
@@ -30,11 +34,58 @@ export default function Checkout({ params: { id } }) {
         setLoading(false);
       }
     };
+
+    const fetchCities = async () => {
+      try {
+        const res = await fetch(`http://localhost:5000/api/cities`, {
+          method: "GET",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          credentials: "include",
+        });
+        const data = await res.json();
+        setCitiesList(data.city);
+      } catch (error) {
+        console.log(error.message);
+      }
+    };
+
+    const fetchBio = async () => {
+      try {
+        const res = await fetch(`http://localhost:5000/api/users/bio`, {
+          method: "GET",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          credentials: "include",
+        });
+        const data = await res.json();
+        setBioList(data.users);
+      } catch (error) {
+        console.log(error.message);
+      }
+    };
+
     fetchCheckouts();
+    fetchCities();
+    fetchBio();
   }, [id]);
+
+  const handleCityChange = e => {
+    setSelectedCity(e.target.value);
+  };
+
+  const handleAddressChange = e => {
+    setFullAddress(e.target.value);
+  };
 
   const handleSubmitAddress = e => {
     e.preventDefault();
+    setBioList(prevBioList => ({
+      ...prevBioList,
+      full_address: `${fullAddress}, ${selectedCity}`, // Menggabungkan city.id dengan alamat penuh
+    }));
     document.getElementById("my_modal_1").close();
   };
 
@@ -56,8 +107,8 @@ export default function Checkout({ params: { id } }) {
         <IoLocation className="text-xl" />
         <div className="text-sm flex-1">
           <p className="pb-1 font-semibold">Delivery Address</p>
-          <p>Fahlan</p>
-          <p>Kel Belian, Batam</p>
+          <p>{bioList.name}</p>
+          {bioList && <p>{`${bioList.full_address}`}</p>}
         </div>
         <button
           onClick={openEditAddress}
@@ -65,53 +116,77 @@ export default function Checkout({ params: { id } }) {
           Edit
         </button>
       </div>
+
       {/* Mobile */}
-      {/* <div className="flex flex-col gap-3 mb-36 md:hidden">
-        {checkoutList.length > 0 ? (
-          checkoutList.map(item => (
+      <div className="flex flex-col gap-3 mb-36 md:hidden">
+        {checkoutList &&
+          checkoutList.length > 0 &&
+          checkoutList.map(listing => (
             // List product checkout
             <div
-              key={item.id}
+              key={listing.id}
               className="p-3 bg-white rounded-md shadow-md">
-              <div className="flex text-sm gap-3 mb-2">
-                <img
-                  src={item.product_image}
-                  width={96}
-                  height={96}
-                  alt={item.product}
-                />
-                <div className="flex flex-col gap-1.5 flex-1 justify-between">
-                  <h1 className="font-semibold line-clamp-2">{item.product}</h1>
-                  <div className="flex flex-col gap-1">
-                    <p className="text-orange-700 font-semibold">
-                      <span>Rp </span>
-                      {item.price}
-                    </p>
-                    <div className="flex gap-2">
-                      <p>x{item.quantity}</p>
+              {listing.checkout_item.map(item => (
+                <div key={item.id}>
+                  <div className="flex text-sm gap-3 mb-2">
+                    <img
+                      src={item.product.product_image}
+                      width={96}
+                      height={96}
+                      alt={item.product.name}
+                    />
+                    <div className="flex flex-col gap-1.5 flex-1 justify-between">
+                      <h1 className="font-semibold line-clamp-2">
+                        {item.product.name}
+                      </h1>
+                      <div className="flex flex-col gap-1">
+                        <p className="text-orange-700 font-semibold">
+                          <span>Rp </span>
+                          {item.product.price.toLocaleString("id-ID", {
+                            minimumFractionDigits: 0,
+                            maximumFractionDigits: 0,
+                          })}
+                        </p>
+                        <div className="flex gap-2">
+                          <p>x{item.quantity}</p>
+                        </div>
+                      </div>
                     </div>
                   </div>
+                  <div className="p-1 flex flex-row justify-between items-center text-xs bg-green-100 rounded-md border-dashed border-2 border-green-200">
+                    <h1 className="font-semibold">Shiping Service</h1>
+                    <div className="flex items-center min-w-36 gap-2">
+                      <p className="font-semibold w-full">
+                        Rp{" "}
+                        {listing.shippingCheckout.price.toLocaleString(
+                          "id-ID",
+                          {
+                            minimumFractionDigits: 0,
+                            maximumFractionDigits: 0,
+                          }
+                        )}
+                      </p>
+                      <p className="px-3 py-1 uppercase border font-semibold border-green-300 rounded-md">
+                        {listing.shippingCheckout.service}
+                      </p>
+                    </div>
+                  </div>
+                  <div className="flex flex-row justify-between text-sm font-bold pt-2 text-orange-700">
+                    <p>Subtotal Product</p>
+                    <p>
+                      Rp{" "}
+                      {item.total_specific_price.toLocaleString("id-ID", {
+                        minimumFractionDigits: 0,
+                        maximumFractionDigits: 0,
+                      })}
+                    </p>
+                  </div>
                 </div>
-              </div>
-              <div className="p-1 flex flex-row justify-between items-center text-xs bg-green-100 rounded-md border-dashed border-2 border-green-200">
-                <h1 className="font-semibold">Shiping Service</h1>
-                <div className="flex items-center min-w-36 gap-2">
-                  <p className="font-semibold w-full">Rp 40.000</p>
-                  <p className="px-3 py-1 border font-semibold border-green-300 rounded-md">
-                    Reguler
-                  </p>
-                </div>
-              </div>
-              <div className="flex flex-row justify-between text-sm font-bold pt-2 text-orange-700">
-                <p>Subtotal Product</p>
-                <p>Rp {item.total_price}</p>
-              </div>
+              ))}
             </div>
-          ))
-        ) : (
-          <p className="text-center">No Order</p>
-        )}
-      </div> */}
+          ))}
+      </div>
+
       {/* Desktop */}
       <div className="hidden md:block">
         <div className="flex items-center bg-orange-600 px-4 text-white font-semibold py-2 rounded-md shadow-md">
@@ -162,9 +237,15 @@ export default function Checkout({ params: { id } }) {
                 <div className="p-1 flex flex-row justify-between items-center text-sm mt-2 bg-green-100 rounded-md border-dashed border-2 border-green-200">
                   <h1 className="font-semibold">Shiping Service</h1>
                   <div className="flex min-w-44 items-center">
-                    <p className="font-semibold w-full">Rp 40.000</p>
-                    <p className="px-3 py-1 border font-semibold border-green-300 rounded-md">
-                      Reguler
+                    <p className="font-semibold w-full">
+                      Rp{" "}
+                      {listing.shippingCheckout.price.toLocaleString("id-ID", {
+                        minimumFractionDigits: 0,
+                        maximumFractionDigits: 0,
+                      })}
+                    </p>
+                    <p className="px-3 uppercase py-1 border font-semibold border-green-300 rounded-md">
+                      {listing.shippingCheckout.service}
                     </p>
                   </div>
                 </div>
@@ -172,6 +253,7 @@ export default function Checkout({ params: { id } }) {
             ))}
         </div>
       </div>
+
       {/* Voucher Code and Total price */}
       <div className="shadow-t w-full right-0 px-4 py-3 mt-4 fixed bg-white bottom-0">
         <div className="xl:max-w-6xl flex mb-3 items-center justify-between gap-6 !text-sm md:text-base w-full mx-auto">
@@ -222,6 +304,7 @@ export default function Checkout({ params: { id } }) {
           </div>
         </div>
       </div>
+
       {/* Modal Edit Address */}
       <dialog
         id="my_modal_1"
@@ -234,35 +317,30 @@ export default function Checkout({ params: { id } }) {
                 <select
                   id="city"
                   name="city"
+                  onChange={handleCityChange}
                   className="block w-full p-2 bg-white border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-orange-500 focus:border-orange-500 focus:ring-1 sm:text-sm">
-                  {[
-                    "Batam",
-                    "Jakarta",
-                    "Bandung",
-                    "Lampung",
-                    "Yogyakarta",
-                    "Semarang",
-                  ].map(city => (
-                    <option
-                      key={city}
-                      value={city}>
-                      {city}
-                    </option>
-                  ))}
+                  {citiesList
+                    .sort((a, b) => a.name.localeCompare(b.name)) // Mengurutkan berdasarkan nama kota
+                    .map(city => (
+                      <option
+                        key={city.id}
+                        value={city.name}>
+                        {city.name}
+                      </option>
+                    ))}
                 </select>
               </div>
             </div>
             <input
               type="text"
               placeholder="Full Address"
+              onChange={handleAddressChange}
               className="w-full text-sm px-3 py-1.5 border border-gray-300 rounded-md focus:outline-none focus:ring-1 focus:ring-orange-500 focus:border-orange-500"
             />
             <div className="modal-action">
               {/* if there is a button in form, it will close the modal */}
               <button
-                onClick={e => {
-                  handleSubmitAddress(e);
-                }}
+                onClick={handleSubmitAddress}
                 type="submit"
                 className="btn text-sm w-full hover:opacity-70 shadow-md duration-300 bg-gradient-to-b from-orange-700 to-orange-600 text-white px-3 py-1 min-h-0 h-10 rounded-md">
                 Simpan
@@ -274,64 +352,3 @@ export default function Checkout({ params: { id } }) {
     </main>
   );
 }
-
-// const cartList = [
-  //   {
-  //     id: 1,
-  //     product:
-  //       "The Sliding Mr. Bones (Next Stop, Pottersville) Happy Birthday To You And For Me too",
-  //     price: 20000,
-  //     quantity: 2,
-  //     total_price: 40000,
-  //     product_image:
-  //       "https://images.unsplash.com/photo-1519689680058-324335c77eba?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-  //   },
-  //   {
-  //     id: 2,
-  //     product: "Witchy Woman",
-  //     price: 30000,
-  //     quantity: 3,
-  //     total_price: 40000,
-  //     product_image:
-  //       "https://images.unsplash.com/photo-1519689680058-324335c77eba?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-  //   },
-  //   {
-  //     id: 3,
-  //     product: "Earth, Wind, and Fire",
-  //     price: 40000,
-  //     quantity: 4,
-  //     total_price: 40000,
-  //     product_image:
-  //       "https://images.unsplash.com/photo-1519689680058-324335c77eba?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-  //   },
-  //   {
-  //     id: 4,
-  //     product:
-  //       "The Sliding Mr. Bones (Next Stop, Pottersville) Happy Birthday To You And For Me too",
-  //     price: 20000,
-  //     quantity: 2,
-  //     total_price: 40000,
-  //     product_image:
-  //       "https://images.unsplash.com/photo-1519689680058-324335c77eba?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-  //   },
-  //   {
-  //     id: 5,
-  //     product:
-  //       "The Sliding Mr. Bones (Next Stop, Pottersville) Happy Birthday To You And For Me too",
-  //     price: 20000,
-  //     quantity: 2,
-  //     total_price: 40000,
-  //     product_image:
-  //       "https://images.unsplash.com/photo-1519689680058-324335c77eba?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-  //   },
-  //   {
-  //     id: 6,
-  //     product:
-  //       "The Sliding Mr. Bones (Next Stop, Pottersville) Happy Birthday To You And For Me too",
-  //     price: 20000,
-  //     quantity: 2,
-  //     total_price: 40000,
-  //     product_image:
-  //       "https://images.unsplash.com/photo-1519689680058-324335c77eba?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-  //   },
-  // ];
