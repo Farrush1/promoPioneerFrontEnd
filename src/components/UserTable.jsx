32f7661cdfe76677979249d50dcd403f480f0de5
DESKTOP-IB5SN3J\Weli Erlina <welierlina08@gmail.com>
2024-05-24 00:21:53 +0700

    weli filter page and user

--- src/components/UserTable.jsx
+++ src/components/UserTable.jsx
@@ -1,12 +1,38 @@
-import React from "react";
+"use client";
+
+import React, { useState, useEffect } from 'react';
+import { useRouter } from 'next/navigation';
 
 const UserTable = () => {
-  const renderActionBox = () => {
-    return (
-      <div className="flex justify-center items-center h-full">
-        <input type="checkbox" className="checkbox" />
-      </div>
-    );
+  const [users, setUsers] = useState([]);
+  const router = useRouter();
+
+  useEffect(() => {
+    const fetchUsers = async () => {
+      try {
+        const response = await fetch('http://localhost:5000/api/users');
+        if (!response.ok) {
+          throw new Error('Failed to fetch users');
+        }
+        const data = await response.json();
+
+        if (data && Array.isArray(data.users)) {
+          setUsers(data.users);
+        } else {
+          console.error('Fetched data is not an array:', data);
+          setUsers([]);
+        }
+      } catch (error) {
+        console.error('Error fetching users:', error);
+        setUsers([]); 
+      }
+    };
+
+    fetchUsers();
+  }, []);
+
+  const handleViewUser = (userId) => {
+    router.push(`/dashboard/user/${userId}`);
   };
 
   return (
@@ -14,127 +40,57 @@ const UserTable = () => {
       <table className="min-w-full divide-y divide-gray-200 shadow-md bg-white">
         <thead className="bg-gray-50 text-center">
           <tr>
-            <th className="px-6 py-3  text-xs font-medium text-gray-500 uppercase tracking-wider">
+            <th className="px-6 py-3 text-xs font-medium text-gray-500 uppercase tracking-wider">
               User ID
             </th>
-            <th className="px-6 py-3  text-xs font-medium text-gray-500 uppercase tracking-wider">
+            <th className="px-6 py-3 text-xs font-medium text-gray-500 uppercase tracking-wider">
               Name
             </th>
-            <th className="px-6 py-3  text-xs font-medium text-gray-500 uppercase tracking-wider">
+            <th className="px-6 py-3 text-xs font-medium text-gray-500 uppercase tracking-wider">
               Address
             </th>
-            <th className="px-6 py-3  text-xs font-medium text-gray-500 uppercase tracking-wider">
-              Phone Number
+            <th className="px-6 py-3 text-xs font-medium text-gray-500 uppercase tracking-wider">
+              Email
             </th>
-            <th className="px-6 py-3  text-xs font-medium text-gray-500 uppercase tracking-wider">
-              City
+            <th className="px-6 py-3 text-xs font-medium text-gray-500 uppercase tracking-wider">
+              View
             </th>
-            <th className="px-6 py-3  text-xs font-medium text-gray-500 uppercase tracking-wider">
+            <th className="px-6 py-3 text-xs font-medium text-gray-500 uppercase tracking-wider">
               Action
             </th>
           </tr>
         </thead>
         <tbody className="divide-y divide-gray-200 text-center">
-          <tr className="hover:bg-gray-100">
-            <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-900">
-              1
-            </td>
-            <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-900">
-              Nama 1
-            </td>
-            <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-900">
-              Indonesia
-            </td>
-            <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-900">
-              +62 999999999
-            </td>
-            <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-900">
-              <button className="btn bg-[#ea580c] text-white">View</button>
-            </td>
-            <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-900">
-              {renderActionBox()}
-            </td>
-          </tr>
-          <tr className="hover:bg-gray-100">
-            <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-900">
-              2
-            </td>
-            <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-900">
-              Nama 2
-            </td>
-            <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-900">
-              Indonesia
-            </td>
-            <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-900">
-              +62 999999999
-            </td>
-            <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-900">
-              <button className="btn bg-[#ea580c] text-white">View</button>
-            </td>
-            <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-900">
-              {renderActionBox()}
-            </td>
-          </tr>
-          <tr className="hover:bg-gray-100">
-            <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-900">
-              3
-            </td>
-            <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-900">
-              Nama 3
-            </td>
-            <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-900">
-              Indonesia
-            </td>
-            <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-900">
-              +62 999999999
-            </td>
-            <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-900">
-              <button className="btn bg-[#ea580c] text-white">View</button>
-            </td>
-            <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-900">
-              {renderActionBox()}
-            </td>
-          </tr>
-          <tr className="hover:bg-gray-100">
-            <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-900">
-              4
-            </td>
-            <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-900">
-              Nama 4
-            </td>
-            <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-900">
-              Indonesia
-            </td>
-            <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-900">
-              +62 999999999
-            </td>
-            <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-900">
-              <button className="btn bg-[#ea580c] text-white">View</button>
-            </td>
-            <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-900">
-              {renderActionBox()}
-            </td>
-          </tr>
-          <tr className="hover:bg-gray-100">
-            <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-900">
-              5
-            </td>
-            <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-900">
-              Nama 5
-            </td>
-            <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-900">
-              Indonesia
-            </td>
-            <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-900">
-              +62 999999999
-            </td>
-            <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-900">
-              <button className="btn bg-[#ea580c] text-white">View</button>
-            </td>
-            <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-900">
-              {renderActionBox()}
-            </td>
-          </tr>
+          {Array.isArray(users) && users.length > 0 ? (
+            users.map((user) => (
+              <tr key={user.id} className="hover:bg-gray-100">
+                <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-900">
+                  {user.id}
+                </td>
+                <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-900">
+                  {user.name}
+                </td>
+                <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-900">
+                  {user.full_address}
+                </td>
+                <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-900">
+                  {user.email}
+                </td>
+                <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-900">
+                  <button className="btn bg-[#ea580c] text-white ml-2" onClick={() => handleViewUser(user.id)}>View</button>
+                </td>
+                <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-900">
+                  <input type="checkbox" className="checkbox" />
+                </td>
+              </tr>
+            ))
+          ) : (
+            <tr>
+              <td colSpan="6" className="px-6 py-4 whitespace-nowrap text-sm text-gray-900">
+                No users found
+              </td>
+            </tr>
+          )}
         </tbody>
       </table>
     </div>
