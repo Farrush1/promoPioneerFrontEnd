74f37231c59bcc67b7f8bb27ac8561dd3b17d334
fahlanlogic <alanfahdi@gmail.com>
2024-05-24 22:41:42 +0700

    ui : fix ui grid homepage

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -3,11 +3,11 @@ import Carousel from "@/components/Carousel";
 
 export default function Home() {
   return (
-    <main className="h-[9999px] xl:max-w-6xl mx-auto px-4 xl:px-0 ">
+    <main className="xl:max-w-6xl pt-16 mx-auto px-4 xl:px-0 ">
       <Carousel />
 
       {/* contoh penggunaan card menggunakan grid */}
-      <div className="grid xl:grid-flow-col grid-cols-2 auto-cols-max gap-3 sm:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5">
+      <div className="grid xl:grid-flow-row grid-rows-2-2 auto-cols-max gap-3 sm:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5">
         <CardProduct />
       </div>
       <h1>Home</h1>
