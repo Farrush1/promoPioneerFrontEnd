8e3ab6411aa35d8230eb8e9fb1219183f9829d22
fahlanlogic <109933376+fahlanlogic@users.noreply.github.com>
2024-05-24 20:05:53 +0700

    weli new (#18)
    Co-authored-by: DESKTOP-IB5SN3J\Weli Erlina <welierlina08@gmail.com>

--- src/components/UserTable.jsx
+++ src/components/UserTable.jsx
@@ -1,6 +1,33 @@
-import React from "react";
+import React, { useState, useEffect } from 'react';
 
 const UserTable = () => {
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    const fetchUsers = async () => {
+      try {
+        const response = await fetch('http://localhost:5000/api/users');
+        if (!response.ok) {
+          throw new Error('Failed to fetch users');
+        }
+        const data = await response.json();
+
+        
+        if (data && Array.isArray(data.users)) {
+          setUsers(data.users);
+        } else {
+          console.error('Fetched data is not an array:', data);
+          setUsers([]);
+        }
+      } catch (error) {
+        console.error('Error fetching users:', error);
+        setUsers([]); 
+      }
+    };
+
+    fetchUsers();
+  }, []);
+
   const renderActionBox = () => {
     return (
       <div className="flex justify-center items-center h-full">
@@ -14,127 +41,56 @@ const UserTable = () => {
       <table className="min-w-full divide-y divide-gray-200 shadow-md bg-white">
         <thead className="bg-gray-50 text-center">
           <tr>
-            <th className="px-6 py-3  text-xs font-medium text-gray-500 uppercase tracking-wider">
+            <th className="px-6 py-3 text-xs font-medium text-gray-500 uppercase tracking-wider">
               User ID
             </th>
-            <th className="px-6 py-3  text-xs font-medium text-gray-500 uppercase tracking-wider">
+            <th className="px-6 py-3 text-xs font-medium text-gray-500 uppercase tracking-wider">
               Name
             </th>
-            <th className="px-6 py-3  text-xs font-medium text-gray-500 uppercase tracking-wider">
+            <th className="px-6 py-3 text-xs font-medium text-gray-500 uppercase tracking-wider">
               Address
             </th>
-            <th className="px-6 py-3  text-xs font-medium text-gray-500 uppercase tracking-wider">
-              Phone Number
+            <th className="px-6 py-3 text-xs font-medium text-gray-500 uppercase tracking-wider">
+              Email
             </th>
-            <th className="px-6 py-3  text-xs font-medium text-gray-500 uppercase tracking-wider">
-              City
-            </th>
-            <th className="px-6 py-3  text-xs font-medium text-gray-500 uppercase tracking-wider">
+            
+            <th className="px-6 py-3 text-xs font-medium text-gray-500 uppercase tracking-wider">
               Action
             </th>
           </tr>
         </thead>
         <tbody className="divide-y divide-gray-200 text-center">
-          <tr className="hover:bg-gray-100">
-            <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-900">
-              1
-            </td>
-            <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-900">
-              Nama 1
-            </td>
-            <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-900">
-              Indonesia
-            </td>
-            <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-900">
-              +62 999999999
-            </td>
-            <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-900">
-              <button className="btn bg-[#ea580c] text-white">View</button>
-            </td>
-            <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-900">
-              {renderActionBox()}
-            </td>
-          </tr>
-          <tr className="hover:bg-gray-100">
-            <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-900">
-              2
-            </td>
-            <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-900">
-              Nama 2
-            </td>
-            <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-900">
-              Indonesia
-            </td>
-            <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-900">
-              +62 999999999
-            </td>
-            <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-900">
-              <button className="btn bg-[#ea580c] text-white">View</button>
-            </td>
-            <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-900">
-              {renderActionBox()}
-            </td>
-          </tr>
-          <tr className="hover:bg-gray-100">
-            <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-900">
-              3
-            </td>
-            <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-900">
-              Nama 3
-            </td>
-            <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-900">
-              Indonesia
-            </td>
-            <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-900">
-              +62 999999999
-            </td>
-            <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-900">
-              <button className="btn bg-[#ea580c] text-white">View</button>
-            </td>
-            <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-900">
-              {renderActionBox()}
-            </td>
-          </tr>
-          <tr className="hover:bg-gray-100">
-            <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-900">
-              4
-            </td>
-            <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-900">
-              Nama 4
-            </td>
-            <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-900">
-              Indonesia
-            </td>
-            <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-900">
-              +62 999999999
-            </td>
-            <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-900">
-              <button className="btn bg-[#ea580c] text-white">View</button>
-            </td>
-            <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-900">
-              {renderActionBox()}
-            </td>
-          </tr>
-          <tr className="hover:bg-gray-100">
-            <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-900">
-              5
-            </td>
-            <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-900">
-              Nama 5
-            </td>
-            <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-900">
-              Indonesia
-            </td>
-            <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-900">
-              +62 999999999
-            </td>
-            <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-900">
-              <button className="btn bg-[#ea580c] text-white">View</button>
-            </td>
-            <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-900">
-              {renderActionBox()}
-            </td>
-          </tr>
+          {Array.isArray(users) && users.length > 0 ? (
+            users.map((user) => (
+              <tr key={user.id} className="hover:bg-gray-100">
+                <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-900">
+                  {user.id}
+                </td>
+                <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-900">
+                  {user.name}
+                </td>
+                <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-900">
+                  {user.full_address}
+                </td>
+                <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-900">
+                  {user.email}
+                </td>
+                
+                <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-900">
+                  <button className="btn bg-[#ea580c] text-white">View</button>
+                </td>
+                <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-900">
+                  {renderActionBox()}
+                </td>
+              </tr>
+            ))
+          ) : (
+            <tr>
+              <td colSpan="7" className="px-6 py-4 whitespace-nowrap text-sm text-gray-900">
+                No users found
+              </td>
+            </tr>
+          )}
         </tbody>
       </table>
     </div>
