ec0dd62c8d8e142969718fa76e15ea727b1abc9e
fahlanlogic <alanfahdi@gmail.com>
2024-05-26 15:53:45 +0700

    feat : complete login page

--- src/app/login/page.jsx
+++ src/app/login/page.jsx
@@ -3,20 +3,20 @@ import { useState } from "react";
 import toast, { Toaster } from "react-hot-toast";
 import { useRouter } from "next/navigation";
 import Link from "next/link";
+import { useCookies } from "react-cookie";
 
-export default function RegisterPage() {
+export default function LoginPage() {
   const [formData, setFormData] = useState({
-    name: "",
     email: "",
     password: "",
-    confirmPassword: "",
-    affiliateCode: "",
   });
   const router = useRouter();
+  const [_cookies, setCookies, _removeCookie] = useCookies(["accessToken"]); // untuk simpan cookies
 
-  const register = async () => {
+  // fetch post login
+  const login = async () => {
     try {
-      const res = await fetch("http://localhost:5000/api/auth/register", {
+      const res = await fetch("http://localhost:5000/api/auth/login", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
@@ -25,34 +25,39 @@ export default function RegisterPage() {
       });
 
       const data = await res.json();
-
       if (!res.ok) {
         toast.error(data.message);
-        return false;
+        return;
       }
-
-      return true;
+      if (data.role === "ADMIN") {
+        setCookies("accessToken", data.accessToken);
+        toast.success("You are logged in as ADMIN", { duration: 3000 });
+        setTimeout(() => {
+          router.push("/dashboard");
+        }, 3000);
+      } else {
+        setCookies("accessToken", data.accessToken);
+        toast.success("Successful login", { duration: 3000 });
+        setTimeout(() => {
+          router.push("/");
+        }, 3000);
+      }
     } catch (error) {
       console.error(error);
       toast.error("An error occurred. Please try again.");
-      return false;
     }
   };
 
+  // simpan nilai input
   const handleChange = async e => {
     const { name, value } = e.target;
     setFormData(prevData => ({ ...prevData, [name]: value }));
   };
 
+  // handle submit
   const handleSubmit = async e => {
     e.preventDefault();
-    const success = await register();
-    if (success) {
-      toast.success("Successfully created account", { duration: 2000 });
-      setTimeout(() => {
-        router.push("/login");
-      }, 2000);
-    }
+    login();
   };
 
   return (
@@ -62,17 +67,17 @@ export default function RegisterPage() {
         reverseOrder={false}
         containerStyle={{ marginTop: "65px" }}
       />
-      <div className="max-w-md w-full space-y-8">
+      <div className="max-w-md w-full space-y-16">
         <div>
           <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">
-            Create an account
+            Login to your account
           </h2>
           <p className="mt-2 text-center text-sm text-gray-600">
             or
             <Link
-              href="/login"
+              href="/register"
               className="text-blue-500 hover:text-blue-600hover:text-indigo-500 px-2">
-              Login
+              Register
             </Link>
           </p>
         </div>
@@ -81,15 +86,6 @@ export default function RegisterPage() {
           onSubmit={handleSubmit}
           className="space-y-8">
           <div className="flex flex-col gap-4">
-            <input
-              name="name"
-              type="name"
-              required
-              className="appearance-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500
-                text-gray-900 rounded-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 text-sm md:text-base"
-              placeholder="Full Name"
-              onChange={handleChange}
-            />
             <input
               name="email"
               type="email"
@@ -108,30 +104,27 @@ export default function RegisterPage() {
               placeholder="Password"
               onChange={handleChange}
             />
-            <input
-              name="confirmPassword"
-              type="password"
-              required
-              className="appearance-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500
-                text-gray-900 rounded-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 text-sm md:text-base"
-              placeholder="Confirm Password"
-              onChange={handleChange}
-            />
-            <input
-              name="affiliateCode"
-              type="text"
-              className="appearance-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500
-                text-gray-900 rounded-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 text-sm md:text-base"
-              placeholder="Affiliate Code"
-              onChange={handleChange}
-            />
           </div>
-
+          {/* <div>
+            <div className="flex items-center justify-between">
+              <div className="flex items-center">
+                <input
+                  type="checkbox"
+                  className="h-5 w-5 text-indigo-600 focus:ring-indigo-500 border-gray-300 rounded-md"
+                />
+                <label
+                  htmlFor=""
+                  className="ml-2 block text-sm text-gray-900">
+                  Remember me
+                </label>
+              </div>
+            </div>
+          </div> */}
           <div>
             <button
               type="submit"
               className="w-full flex justify-center items-center px-4 py-2 bg-gradient-to-l from-orange-600 to-orange-500 hover:bg-orange-700 text-white font-bold rounded-md shadow-md hover:translate-x-1 hover:translate-y-1 duration-300">
-              Register
+              Login
             </button>
           </div>
         </form>
