42b2fced7d9489d935390a0a35532564462d095c
fahlanlogic <alanfahdi@gmail.com>
2024-05-27 20:08:14 +0700

    ui : fixing style and config card product components

--- src/components/CardProduct.jsx
+++ src/components/CardProduct.jsx
@@ -6,56 +6,45 @@
 import Link from "next/link";
 import { useEffect, useState } from "react";
 
-export default function CardProduct({ key, name, price, image }) {
-  const [productListing, setProductListing] = useState([]);
-  const [loading, setLoading] = useState(false);
-  const [error, setError] = useState(null);
-
-  // useEffect(() => {
-  //   fetchProducts();
-  // }, []);
-
-  // const fetchProducts = async () => {
-  //   try {
-  //     const response = await fetch("http://localhost:5000/api/products");
-  //     if (!response.ok) {
-  //       throw new Error(`Error: ${response.status}`);
-  //     }
-  //     const data = await response.json();
-  //     console.log("Fetched data:", data);  // Add this line to log the fetched data
-  //     setProductListing(data.products);
-  //     setLoading(false);
-  //   } catch (error) {
-  //     console.error("Error fetching products:", error);
-  //     setError(error.message);
-  //     setLoading(false);
-  //   }
-  // };
-
-  if (loading) {
-    return <p>Loading...</p>;
-  }
-
-  if (error) {
-    return <p>Error: {error}</p>;
-  }
-
+export default function CardProduct({
+  key,
+  name,
+  price,
+  qty,
+  image,
+  className,
+  promo,
+}) {
   return (
     <>
       <Link
         key={key}
         href={`/product/${key}`}
-        className="shadow-sm w-full rounded-md border border-slate-200 hover:shadow-md duration-300 hover:-translate-x-1 hover:-translate-y-1">
+        className={`shadow-sm w-full relative rounded-md border border-slate-200 ${className} hover:shadow-md duration-300 hover:-translate-x-1 hover:-translate-y-1`}>
+        {promo && (
+          <span className="absolute top-0 text-xs font-bold py-1 left-0 bg-gradient-to-t from-orange-600 to-orange-500 text-white px-2 rounded-br-md">
+            {/* opsional kalo product promo */}
+            {promo}
+          </span>
+        )}
         <img
           src={image}
           className="w-full h-52 sm:h-56 lg:h-64 object-cover rounded-t-sm"
         />
-        <div className="p-1.5 flex flex-col gap-1 justify-between h-[82px]">
-          <h1 className="text-sm font-medium mb-1.5 line-clamp-2">{name}</h1>
-          <p className="text-sm font-semibold text-orange-600">
-            <span>Rp </span>
-            {price}
-          </p>
+        <div className="p-1.5 flex flex-col gap-1 justify-between md:h-24">
+          <h1 className="text-sm md:text-base flex-1 font-semibold mb-1.5 line-clamp-2">
+            {name}
+          </h1>
+          <div className="flex justify-between items-end">
+            <span className="text-sm">x{qty}</span>
+            <p className="font-semibold text-orange-600">
+              <span className="text-sm md:text-base">Rp </span>
+              {price.toLocaleString("id-ID", {
+                minimumFractionDigits: 0,
+                maximumFractionDigits: 0,
+              })}
+            </p>
+          </div>
         </div>
       </Link>
     </>
